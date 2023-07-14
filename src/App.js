4caa1d6502886b84e87053297992d305e97ac338
Batuhan Güleç <batuhan.gulec170@gmail.com>
2023-07-14 13:51:14 +0300

    change1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
+import './style.css';
+import {Title} from "./Components";
 
 function App() {
-  return (
-    <div className="App">
-        <img src="/logo192.png" alt=""/>
-        <img src={logo} alt=""/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Title>React App</Title>
+            <img className="logo1" src="/logo192.png" alt=""/>
+            <img src={logo} alt=""/>
+        </div>
+    );
 }
 
 export default App;
